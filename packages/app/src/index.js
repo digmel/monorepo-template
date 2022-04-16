54d797bb4a3de50932740f83ed283b2e2f6f6ad1
George <gio.digmel@gmail.com>
2022-04-15 22:12:44 -0700

    added shared dir

---
+++ packages/app/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "green",
+        justifyContent: "center",
+        alignContent: "center",
+      }}
+    >
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+export default App;
